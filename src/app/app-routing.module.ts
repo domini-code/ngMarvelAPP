3ef87f61b3c1ad2e1962690d7ae69924b1bbf69f
Bezael <bezael_e@hotmail.com>
2019-05-25 12:51:53 +0200

    Temp route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharactersComponent } from './characters/characters.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', redirecTo: '/characters', pathMatch: 'full'
+  },
+  {
+    path: 'characters', component: CharactersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
